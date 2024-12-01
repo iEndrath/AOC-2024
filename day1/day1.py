d34eb3800b1e0febcbe5944b4d9d5bda52834fe0
iEndrath <146292874+iEndrath@users.noreply.github.com>
2024-12-01 17:27:32 +0100

    cleaned it up some more :3

--- day1/day1.py
+++ day1/day1.py
@@ -5,7 +5,7 @@ def append_numbers(line, left, right):
     right.append(int(right_number))
     return
     
-def main1():
+def main(choice):
     #read and create left & right list
     f = open("day1\input.txt")
     left_list = []
@@ -13,34 +13,24 @@ def main1():
     for line in f:
         append_numbers(line, left_list, right_list)
     
-    total_distance = 0
-    left_list.sort() 
-    right_list.sort()
+    if choice == "1":
+        total_distance = 0
+        left_list.sort() 
+        right_list.sort()
 
-    for i in range(0, len(left_list)):
-        total_distance += max(left_list[i], right_list[i]) - min(left_list[i], right_list[i])
-    
-    print(total_distance)
-
-def main2():
-    #read and create left & right list
-    f = open("day1\input.txt")
-    left_list = []
-    right_list = []
-    for line in f:
-        append_numbers(line, left_list, right_list)
-    
-    similarity_score = 0
-    for i in range(0, len(left_list)):
-        total_appearances = 0
-        for j in range(0, len(right_list)):
-            if left_list[i] == right_list[j]:
-                total_appearances += 1
-        similarity_score += left_list[i] * total_appearances
-    print(similarity_score)
+        for i in range(0, len(left_list)):
+            total_distance += max(left_list[i], right_list[i]) - min(left_list[i], right_list[i])
+        
+        print(total_distance)
+    else:
+        similarity_score = 0
+        for i in range(0, len(left_list)):
+            total_appearances = 0
+            for j in range(0, len(right_list)):
+                if left_list[i] == right_list[j]:
+                    total_appearances += 1
+            similarity_score += left_list[i] * total_appearances
+        print(similarity_score)
 
 choice = input("part 1 or 2? ")
-if choice == "1":
-    main1()
-else:
-    main2()
+main(choice)
